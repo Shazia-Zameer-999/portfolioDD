9e171cf23c0e34aa3d5e7e690198e68eeb9a193d
Daten by SZ <datendiva7867@gmail.com>
2025-10-29 15:38:26 +0530

    project 1 enh'

--- components/HorizontalShowcase.jsx
+++ components/HorizontalShowcase.jsx
@@ -16,7 +16,7 @@ const projects = [
     {
         "title": "Full-Stack Linktree Clone",
         "description": "A dynamic 'link in bio' application where users can claim a unique handle, add a profile picture, and manage a list of their personal links, all stored in a MongoDB database.",
-        "image": "/show1.png",
+        "image": "/show4.png",
         "tech": [
             "Next.js (App Router)",
             "React",
